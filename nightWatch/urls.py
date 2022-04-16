7e3656c8c40935161bed02180eeca98d5a2e69e6
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-04-16 14:18:52 +0300

    Added URLs for django registrations

--- nightWatch/urls.py
+++ nightWatch/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 import neighbourhood
 from django.contrib import admin
 from django.urls import path,include
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth.views import LoginView,LogoutView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('neighbourhood.urls')),
+    path('accounts/register/',
+        RegistrationView.as_view(success_url='/dashboard'),
+        name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', LogoutView.as_view(), {"next_page": '/'}),
+    path('login/', LoginView.as_view(), {"next_page": '/'}),
 ]
