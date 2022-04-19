f7c7cc5086c613ec4f06996555e9d33fc9db1ce8
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-04-19 09:37:59 +0300

    Updated models to the admin

--- neighbourhood/admin.py
+++ neighbourhood/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile, NeighbourHood, Comments, Post, Business
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(NeighbourHood)
+admin.site.register(Comments)
+admin.site.register(Post)
+admin.site.register(Business)
